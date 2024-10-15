83dcd1acd7f009f1d646e6eeda1be7d48ce6f88b
Cramara <cramara@users.noreply.github.com>
2024-10-15 09:19:54 +0200

    setup srping

---
+++ src/main/java/fr/insa/hexanome/OUPS/OupsApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.hexanome.OUPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OupsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/insa/hexanome/OUPS/OupsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.hexanome.OUPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
